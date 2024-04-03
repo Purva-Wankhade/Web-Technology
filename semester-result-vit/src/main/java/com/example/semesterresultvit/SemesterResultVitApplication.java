dc02eab438f5b48cde73eafc0791f54c186e8765
Purva-Wankhade <115718486+Purva-Wankhade@users.noreply.github.com>
2024-04-04 00:53:51 +0530

    Add files via upload

---
+++ semester-result-vit/src/main/java/com/example/semesterresultvit/SemesterResultVitApplication.java
@@ -0,0 +1,13 @@
+package com.example.semesterresultvit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterResultVitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemesterResultVitApplication.class, args);
+	}
+
+}
---
+++ semester-result-vit/src/test/java/com/example/semesterresultvit/SemesterResultVitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semesterresultvit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemesterResultVitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
