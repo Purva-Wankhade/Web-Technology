9a786e0124f7db037c223f4e8fe639b68ecfd326
Purva-Wankhade <115718486+Purva-Wankhade@users.noreply.github.com>
2024-04-04 00:47:29 +0530

    Add files via upload

---
+++ electricity-bill-calculator/src/test/java/com/example/electricitybillcalculator/ElectricityBillCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electricitybillcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityBillCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
