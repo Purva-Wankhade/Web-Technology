d1f015ba60eec5a17594192b9c98ae53c59f2459
Purva-Wankhade <115718486+Purva-Wankhade@users.noreply.github.com>
2024-04-04 09:56:38 +0530

    Add files via upload

---
+++ semester-result-vit/src/main/java/com/example/semesterresultvit/controller/ResultController.java
@@ -0,0 +1,38 @@
+package com.example.semesterresultvit.controller;
+
+import com.example.semesterresultvit.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/result")
+public class ResultController {
+
+    @GetMapping
+    public String getResultForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "result";
+    }
+
+    @PostMapping
+    public String calculateResult(Student student, Model model) {
+        double result1 = (student.getMse1() * 0.3) + (student.getEse1() * 0.7);
+        double result2 = (student.getMse2() * 0.3) + (student.getEse2() * 0.7);
+        double result3 = (student.getMse3() * 0.3) + (student.getEse3() * 0.7);
+        double result4 = (student.getMse4() * 0.3) + (student.getEse4() * 0.7);
+
+       
+        double totalPercentage = (result1 + result2 + result3 + result4) / 4.0;
+
+        model.addAttribute("result1", result1);
+        model.addAttribute("result2", result2);
+        model.addAttribute("result3", result3);
+        model.addAttribute("result4", result4);
+        model.addAttribute("totalPercentage", totalPercentage);
+        return "result";
+    }
+}
+
---
+++ semester-result-vit/src/main/java/com/example/semesterresultvit/model/Student.java
@@ -0,0 +1,86 @@
+package com.example.semesterresultvit.model;
+
+public class Student {
+    private String name;
+    private double mse1;
+    private double ese1;
+    private double mse2;
+    private double ese2;
+    private double mse3;
+    private double ese3;
+    private double mse4;
+    private double ese4;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMse1() {
+        return mse1;
+    }
+
+    public void setMse1(double mse1) {
+        this.mse1 = mse1;
+    }
+
+    public double getEse1() {
+        return ese1;
+    }
+
+    public void setEse1(double ese1) {
+        this.ese1 = ese1;
+    }
+
+    public double getMse2() {
+        return mse2;
+    }
+
+    public void setMse2(double mse2) {
+        this.mse2 = mse2;
+    }
+
+    public double getEse2() {
+        return ese2;
+    }
+
+    public void setEse2(double ese2) {
+        this.ese2 = ese2;
+    }
+
+    public double getMse3() {
+        return mse3;
+    }
+
+    public void setMse3(double mse3) {
+        this.mse3 = mse3;
+    }
+
+    public double getEse3() {
+        return ese3;
+    }
+
+    public void setEse3(double ese3) {
+        this.ese3 = ese3;
+    }
+
+    public double getMse4() {
+        return mse4;
+    }
+
+    public void setMse4(double mse4) {
+        this.mse4 = mse4;
+    }
+
+    public double getEse4() {
+        return ese4;
+    }
+
+    public void setEse4(double ese4) {
+        this.ese4 = ese4;
+    }
+}
